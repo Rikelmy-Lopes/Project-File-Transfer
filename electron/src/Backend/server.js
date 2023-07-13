3fa94fa6675642d844bee43b80c58252d1a45184
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-07-12 22:01:44 -0300

    add electron forge to project

--- electron/src/Backend/server.js
+++ electron/src/Backend/server.js
@@ -5,18 +5,19 @@ let server = null;
 
 function serverHandler(ipcMain) {
   ipcMain.on('start-server', () => {
-    if (!server || !server.listening) {
+    if (!server) {
       server = app.listen(PORT, ip.address(), () => {
         console.log(`Servidor iniciado em http://${ip.address()}:${PORT}/`);
       });
     }
-    
   
   });
   
   ipcMain.on('stop-server', () => {
     if (server && server.listening) {
+      server.closeAllConnections();
       server.close((error) => {
+        server = null;
         if(error) {
           console.log(error);
           return;

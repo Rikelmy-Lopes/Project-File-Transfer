6580690089a2ab0c3924b179cde24958c3e5bbeb
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-07-13 01:30:44 -0300

    add electron forge to project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../electron/src/backend/dist/'
+    outDir: '../electron/src/Backend/dist/'
   }
 })

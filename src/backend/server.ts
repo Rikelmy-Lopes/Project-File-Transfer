283bc8241f26ca6fd49fc1fb4f549535359983bf
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-09-21 14:43:00 -0300

    refactor: MainWindow e serverHandle para classes

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,34 +1,49 @@
 import { IpcMain } from 'electron';
+import { Application } from 'express';
 import { Server } from 'http';
 import ip from 'ip';
-import app from './app';
-let server: Server | null = null;
 
-
-function serverHandler(ipcMain: IpcMain) {
-  ipcMain.on('start-server', async (_, port: number) => {
-    if (!server) {
-      server = app.listen(port, ip.address(), () => {
-        console.log(`Servidor iniciado em http://${ip.address()}:${port}`);
-      });
-    }
-  
-  });
-  
-  ipcMain.on('stop-server', () => {
-    if (server && server.listening) {
-      server.closeAllConnections();
-      server.close((error) => {
-        server = null;
-        if(error) {
-          console.log(error);
-          return;
-        }
-        console.log('Server closed');
-      });
-      server = null;
-    }
-  });
+export interface IServerHandler {
+  start(ipcMain: IpcMain): void;
+  stop(ipcMain: IpcMain): void;
 }
 
-export default serverHandler;
+
+export class ServerHandler implements IServerHandler {
+  private app: Application;
+  private server: Server | null;
+
+  constructor(expressApp: Application) {
+    this.app = expressApp;
+    this.server = null;
+  }
+
+  public start(ipcMain: IpcMain) {
+    ipcMain.on('start-server', async (_, port: number) => {
+      if (!this.server) {
+        this.server = this.app.listen(port, ip.address(), () => {
+          console.log(`Servidor iniciado em http://${ip.address()}:${port}`);
+        });
+      }
+    });
+  }
+
+  public stop(ipcMain: IpcMain) {
+    ipcMain.on('stop-server', () => {
+      if (this.server && this.server.listening) {
+        this.server.closeAllConnections();
+        this.server.close((error) => {
+          this.server = null;
+          if(error) {
+            console.log(error);
+            return;
+          }
+          console.log('Server closed');
+        });
+        this.server = null;
+      }
+    });
+  }
+
+}
+
--- src/main/main.ts
+++ src/main/main.ts
@@ -1,58 +1,80 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import { join } from 'path';
-import serverHandler from '../backend/server';
-
-if (!app.requestSingleInstanceLock()) {
-  app.quit()
-  process.exit(1)
-}
+import expressApp from '../backend/app';
+import { IServerHandler, ServerHandler } from '../backend/server';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-let win : BrowserWindow | null = null;
+class MainWindow {
+  private app: Electron.App
+  private ipcMain: Electron.IpcMain
+  private window: Electron.BrowserWindow | null;
+  private serverHandler: IServerHandler
 
-async function createWindow () {
-  win = new BrowserWindow({
-    title: 'Main window',
-    width: 1024,
-    height: 768,
-    webPreferences: {
-      preload: join(__dirname, '../preload/preload.js'),
-      nodeIntegration: true,
-    }
-  })
-
-  if (isDev) {
-    win.loadURL('http://localhost:5173');
-    win.webContents.openDevTools();
-  } else {
-    win.removeMenu();
-    win.loadFile(join(__dirname, '../renderer/index.html'));
+  constructor(app: Electron.App, ipcMain: Electron.IpcMain, serverHandler: IServerHandler) {
+    this.app = app;
+    this.ipcMain = ipcMain;
+    this.window = null;
+    this.serverHandler = serverHandler;
+    this.init();
   }
+
+  private createWindow() {
+    this.window = new BrowserWindow({
+      title: 'Main window',
+      width: 1024,
+      height: 768,
+      webPreferences: {
+        preload: join(__dirname, '../preload/preload.js'),
+        nodeIntegration: true,
+      }
+    })
+    if (isDev) {
+      this.window.loadURL('http://localhost:5173');
+      this.window.webContents.openDevTools();
+    } else {
+      this.window.removeMenu();
+      this.window.loadFile(join(__dirname, '../renderer/index.html'));
+    }
+  }
+
+  private setListeners() {
+    if (!app.requestSingleInstanceLock()) {
+      app.quit()
+      process.exit(1)
+    }
+    this.serverHandler.start(this.ipcMain);
+    this.serverHandler.stop(this.ipcMain);
+    this.app.on('window-all-closed', () => {
+      this.window = null;
+      if (process.platform !== 'darwin') this.app.quit()
+    })
+    app.on('second-instance', () => {
+      if (this.window) {
+        // Focus on the main window if the user tried to open another
+        if (this.window.isMinimized()) this.window.restore()
+        this.window.focus()
+      }
+    })
+    app.on('activate', () => {
+      const allWindows = BrowserWindow.getAllWindows()
+      if (allWindows.length) {
+        allWindows[0].focus()
+      } else {
+        this.init();
+      }
+    })
+  }
+
+  private init() {
+    this.app.whenReady().then(() => {
+      this.createWindow();
+      this.setListeners();
+    });
+  }
+  
 }
 
-app.whenReady().then(createWindow)
+const serverHandler = new ServerHandler(expressApp)
 
-serverHandler(ipcMain)
-
-app.on('window-all-closed', () => {
-  win = null
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('second-instance', () => {
-  if (win) {
-    // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore()
-    win.focus()
-  }
-})
-
-app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows()
-  if (allWindows.length) {
-    allWindows[0].focus()
-  } else {
-    createWindow()
-  }
-})
+new MainWindow(app, ipcMain, serverHandler);
